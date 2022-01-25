43ae5d5880d7e108b5488a4e391a8bb9a440f985
Polina Shneider <beatrixlinkoln@gmail.com>
2022-01-26 00:20:43 +0300

    Refactor a bit

--- dao.py
+++ dao.py
@@ -1,38 +1,20 @@
 import psycopg2
 
-all_entities_select_Query = "SELECT id FROM dataprocessing_items ORDER BY id;"
-all_rpds_select_Query = "SELECT id FROM workprogramsapp_workprogram"
-approved_rpd_outcomes_select_Query = "SELECT workprogram_id, item_id FROM dataprocessing_items di " \
-                                     "JOIN workprogramsapp_outcomesofworkprogram wo on di.id = wo.item_id " \
-                                     "where wo.workprogram_id in " \
-                                     "(SELECT work_program_id FROM workprogramsapp_expertise we " \
-                                     "WHERE we.expertise_status = 'AC' ) " \
-                                     "ORDER BY workprogram_id;"
+from train import calculate_distance
 
-rpd_outcomes_by_id_select_Query = "SELECT workprogram_id, item_id FROM dataprocessing_items di " \
-                                  "JOIN workprogramsapp_outcomesofworkprogram wo on di.id = wo.item_id " \
-                                  "where wo.workprogram_id in " \
-                                  "(SELECT work_program_id FROM workprogramsapp_expertise we " \
-                                  "WHERE we.expertise_status = 'AC' ) " \
-                                  "ORDER BY workprogram_id;"
+all_entities_select_Query = "SELECT id FROM public.dataprocessing_items ORDER BY id;"
+rpd_grouped_features_select_Query = "SELECT wiei.workprogram_id, ARRAY_AGG( wiei.entity_id ORDER BY wiei.workprogram_id, wiei.entity_id ) features FROM (SELECT wo.workprogram_id, di.id as entity_id FROM public.dataprocessing_items di JOIN public.workprogramsapp_outcomesofworkprogram wo on di.id = wo.item_id JOIN public.workprogramsapp_expertise we on we.work_program_id = wo.workprogram_id WHERE we.expertise_status = 'AC' ) as wiei GROUP BY wiei.workprogram_id ORDER BY wiei.workprogram_id LIMIT 1000;"
 
-rpd_outcomes_vector_select_Query = "SELECT id, " \
-                                   "CASE " \
-                                   "WHEN id in (" \
-                                   "SELECT id " \
-                                   "FROM dataprocessing_items di " \
-                                   "WHERE di.id in ( " \
-                                   "SELECT wo.item_id " \
-                                   "FROM workprogramsapp_outcomesofworkprogram wo " \
-                                   "JOIN workprogramsapp_expertise we on we.work_program_id = wo.workprogram_id " \
-                                   "WHERE we.expertise_status = 'AC' " \
-                                   "AND wo.workprogram_id = (%s)" \
-                                   ")" \
-                                   ") THEN 1 " \
-                                   "ELSE 0 " \
-                                   "END feature " \
-                                   "FROM dataprocessing_items " \
-                                   "ORDER BY id;"
+
+def normalize(own_features, all_features):
+    res = []
+    for i in range(len(all_features)):
+        if all_features[i] in own_features:
+            res.append(1)
+        else:
+            res.append(0)
+    return res
+
 
 try:
     connection = psycopg2.connect(user="postgres",
@@ -40,24 +22,29 @@ try:
                                   host="127.0.0.1",
                                   port="5432",
                                   database="analytics_db")
-    cursor = connection.cursor()
-    postgreSQL_select_entities_Query = all_rpds_select_Query
-    postgreSQL_select_Query = rpd_outcomes_vector_select_Query
 
-    cursor.execute(postgreSQL_select_entities_Query)
+    cursor = connection.cursor()
+    cursor.execute(all_entities_select_Query)
+    entities = cursor.fetchall()
+
+    cursor.execute(rpd_grouped_features_select_Query)
+    rpd_features = cursor.fetchall()
     print("Selecting rows from mobile table using cursor.fetchall")
     vector = {}
-    records = cursor.fetchall()
+    all_features = set()
 
     print("Print each row and it's columns values")
-    for row in records:
+    for row in rpd_features:
         rpd_id = row[0]
-        vector[rpd_id] = []
-        cursor.execute(postgreSQL_select_Query, (rpd_id,))
-        data = cursor.fetchall()
-        for item in data:
-            vector[rpd_id].append(item[1])
-    print(vector)
+        features = row[1]
+        all_features.update(features)
+
+    for row in rpd_features:
+        rpd_id = row[0]
+        features = row[1]
+        vector[rpd_id] = normalize(features, sorted(all_features))
+
+    calculate_distance(vector)
 except (Exception, psycopg2.Error) as error:
     print("Error while fetching data from PostgreSQL", error)
 
--- train.py
+++ train.py
@@ -7,28 +7,18 @@ fourth = [1, 0, 1, 0]
 fifth = [1, 0, 0, 0]
 data = [fifth, second, third, fourth, fifth]
 
-max_similarity = 0
-[i, j] = [-1, -1]
 
+def calculate_distance(data):
+    max_similarity = 0
+    [i, j] = [-1, -1]
 
+    for x_idx, x_val in data.items():
+        for y_idx, y_val in data.items():
+            if x_idx == y_idx:
+                continue
+            dot_product = np.dot(x_val, y_val)
+            if dot_product > max_similarity:
+                max_similarity = dot_product
+                [i, j] = [x_idx, y_idx]
 
-
-
-
-
-
-
-
-
-
-
-for x_idx, x_val in enumerate(data):
-    for y_idx, y_val in enumerate(data):
-        if x_idx == y_idx:
-            continue
-        dot_product = np.dot(x_val, y_val)
-        if dot_product > max_similarity:
-            max_similarity = dot_product
-            [i, j] = [x_idx, y_idx]
-
-print(max_similarity, data[i], data[j])
+    print(max_similarity, i, j)
